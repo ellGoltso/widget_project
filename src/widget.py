eadaf6eaf593992737075ebb5f88cd070924ff58
Дмитрий <goltsovi3@gmail.com>
2025-05-04 14:49:16 +0300

    added function get_date

--- src/widget.py
+++ src/widget.py
@@ -20,3 +20,13 @@ def mask_account_card(card_or_account: str) -> str:
         full_name_card_account += card_or_account_type + masks.get_mask_account(int(number))
 
     return full_name_card_account
+
+
+def get_date(date_time: str) -> str:
+    """Получает строку даты и времени и возвращает строку с датой в формате : ДД.ММ.ГГГГ"""
+
+    year: str = date_time[:4]
+    month: str = date_time[5:7]
+    day: str = date_time[8:10]
+
+    return f"{day}.{month}.{year}"
