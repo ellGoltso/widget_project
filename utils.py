7f0147e132b3450e616de991995e5c3dadce9756
Дмитрий <goltsovi3@gmail.com>
2025-06-08 08:57:20 +0300

    create function get_data

---
+++ utils.py
@@ -0,0 +1,21 @@
+import json
+
+def get_data(path: str) -> list[dict]:
+    """ Принимает на вход путь до JSON-файла
+    и возвращает список словарей с данными """
+
+    try:
+        with open(path, encoding = 'utf-8') as f:
+            data = json.load(f)
+    except json.JSONDecodeError:
+        return []
+    except FileNotFoundError:
+        return []
+
+    if isinstance(data, list):
+        return data
+    return []
+
+
+
+print(get_data("data/operations.json"))
